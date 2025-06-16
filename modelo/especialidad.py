55118ae044e510e85cab18b2d037c69b9ce4fdc6
Nico <nve2004@gmail.com>
2025-06-16 17:10:10 -0300

    agrego normmalizacion de los dias de la semana y de las especialidades para evitar futuros problemas

--- modelo/especialidad.py
+++ modelo/especialidad.py
@@ -1,13 +1,20 @@
+import unicodedata
+
 class Especialidad:
     def __init__(self, tipo, dias):
         self.__tipo__ = tipo
-        self.__dias__ = [d.lower() for d in dias]
+        self.__dias__ = [self.__normalizar_dia(d) for d in dias]
 
     def obtener_especialidad(self):
         return self.__tipo__
 
     def verificar_dia(self, dia):
-        return dia.lower() in self.__dias__
+        return self.__normalizar_dia(dia) in self.__dias__
+
+    def __normalizar_dia(self, dia):
+        dia = dia.strip().lower()
+        dia = unicodedata.normalize("NFKD", dia).encode("ascii", "ignore").decode("utf-8")
+        return dia
 
     def __str__(self):
         dias_str = ", ".join(self.__dias__)
--- modelo/medico.py
+++ modelo/medico.py
@@ -1,3 +1,5 @@
+import unicodedata
+
 class Medico:
     def __init__(self, nombre, matricula):
         self.__nombre__ = nombre
@@ -14,10 +16,16 @@ class Medico:
         return self.__especialidades__.copy()
 
     def obtener_especialidad_para_dia(self, dia):
+        dia_normalizado = self.__normalizar_dia(dia)
         for esp in self.__especialidades__:
-            if esp.verificar_dia(dia):
+            if esp.verificar_dia(dia_normalizado):
                 return esp.obtener_especialidad()
         return None
+
+    def __normalizar_dia(self, dia):
+        dia = dia.strip().lower()
+        dia = unicodedata.normalize("NFKD", dia).encode("ascii", "ignore").decode("utf-8")
+        return dia
 
     def __str__(self):
         especialidades_str = "\n  ".join(str(e) for e in self.__especialidades__)
